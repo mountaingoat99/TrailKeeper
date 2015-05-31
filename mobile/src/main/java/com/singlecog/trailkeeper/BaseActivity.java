330a51008b76df9b4cd0b0cc2c7702ec3d3fc741
Jeremey Rodriguez <jeremey.rodriguez@outlook.com>
2015-05-31 13:57:39 -0500

    BaseActivity class is working for the navigation drawer and tool bar items

---
+++ mobile/src/main/java/com/singlecog/trailkeeper/BaseActivity.java
@@ -0,0 +1,152 @@
+package com.singlecog.trailkeeper;
+
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+/**
+ * Created by jeremeyrodriguez on 5/31/15.
+ */
+public class BaseActivity extends AppCompatActivity {
+
+    //Navigation Drawer
+    public DrawerLayout mDrawerLayout;
+    //Navigation Drawer List of items
+    public ListView mDrawerList;
+    public ActionBarDrawerToggle mDrawerToggle;
+    public String mActivityTitle;
+
+    protected void onCreateDrawer() {
+        //Instantiate Navigation Drawer
+        setupNavDrawer();
+        setUpDrawer();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        return id == R.id.action_settings ||
+                mDrawerToggle.onOptionsItemSelected(item) ||
+                super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+
+        // syncs toggle state after onRestoreInstanceState
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    //Set up Navigation Drawer
+    private void setupNavDrawer() {
+
+        //Instantiate Navigation Drawer
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        //Instantiate Navigation Drawer List
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mActivityTitle = getTitle().toString();
+
+        //Populate Navigation Drawer with string.xml values
+        mDrawerList.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_activated_1,
+                new String[]{
+                        getString(R.string.home),
+                        getString(R.string.find_trail)
+                        //getString(R.string.title_section2),
+                        //getString(R.string.title_section3)
+                }));
+
+        //Manage here what happens after item clicked in the Navigation Drawer
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                mDrawerLayout.closeDrawer(GravityCompat.START);
+
+                Handler mHandler = new Handler();
+                mHandler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent;
+                        switch (position) {
+                            case 0:
+
+                                intent = new Intent(BaseActivity.this, Home.class);
+                                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+
+
+                                break;
+                            case 1:
+
+                                intent = new Intent(BaseActivity.this, FindTrail.class);
+                                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+
+                                break;
+//                            case 2:
+//
+//                                intent = new Intent(BaseActivity.this, Teste2.class);
+//                                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+//
+//
+//                                break;
+//                            case 3:
+//
+//                                intent = new Intent(BaseActivity.this, Teste3.class);
+//                                startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION));
+//
+//
+//                                break;
+
+                        }
+                    }
+                }, 200);
+            }
+        });
+    }
+
+    private void setUpDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+}
--- mobile/src/main/java/com/singlecog/trailkeeper/FindTrail.java
+++ mobile/src/main/java/com/singlecog/trailkeeper/FindTrail.java
@@ -1,17 +1,17 @@
 package com.singlecog.trailkeeper;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class FindTrail extends Activity {
+public class FindTrail extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_trail);
+        super.onCreateDrawer();
+
     }
 
     @Override
@@ -20,19 +20,19 @@ public class FindTrail extends Activity {
         getMenuInflater().inflate(R.menu.menu_find_trail, menu);
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
